248b8b25493102b0dea36636bc60ee1ce4d64a7a
vaidehi2503 <shah.vaid@northeastern.edu>
2021-12-01 02:16:30 -0500

    added search api, insert, delete and update apis

--- index.js
+++ index.js
@@ -148,6 +148,304 @@ app.delete("/review/:id", function(request, response){
     });
 });
 
+app.delete("/movie/:id", function(request, response){
+
+    var movie_id = request.params.id;
+
+    connection.query("CALL DeleteMovie(?)", [movie_id], function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "Movie has been deleted successfully",
+                data: result
+            });
+        }
+    });
+});
+
+app.delete("/user/:id", function(request, response){
+
+    var user_id = request.params.id;
+
+    connection.query("CALL DeleteUser(?)", [user_id], function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "User has been deleted successfully",
+                data: result
+            });
+        }
+    });
+});
+
+app.delete("/favorite", function(request, response){
+
+    var user_id = request.body.user_id;
+    var movie_id = request.body.movie_id;
+
+    connection.query("CALL DeleteFavorite(?,?)", [movie_id,user_id], function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "Favorite has been deleted successfully",
+                data: result
+            });
+        }
+    });
+});
+
+app.post("/movie", function(request, response){
+
+    var Name = request.body.Name;
+    var Description = request.body.Description;
+    var releaseDate = request.body.releaseDate;
+    var LongMinutes = request.body.LongMinutes;
+    var ImageUrl = request.body.ImageUrl;
+    var Genre = request.body.Genre;
+    var BoxOffice = request.body.BoxOffice;
+    var Writer = request.body.Writer;
+    var Director = request.body.Director;
+    var Actors = request.body.Actors;
+    var LanguageMovie = request.body.LanguageMovie;
+    var Country = request.body.Country;
+    
+
+    connection.query("CALL InsertMovie(?,?,?,?,?,?,?,?,?,?,?,?)", [Name,Description,releaseDate,LongMinutes,ImageUrl,Genre,BoxOffice,Writer,
+        Director,Actors,LanguageMovie,Country],
+     function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "Movie has been added successfully",
+                data: result
+            });
+        }
+    });
+});
+
+app.put("/movie", function(request, response){
+
+    var p_Name = request.body.Name;
+    var p_Description = request.body.Description;
+    var p_releaseDate = request.body.releaseDate;
+    var p_LongMinutes = request.body.LongMinutes;
+    var p_ImageUrl = request.body.ImageUrl;
+    var p_Genre = request.body.Genre;
+    var p_BoxOffice = request.body.BoxOffice;
+    var p_Writer = request.body.Writer;
+    var p_Director = request.body.Director;
+    var p_Actors = request.body.Actors;
+    var p_LanguageMovie = request.body.LanguageMovie;
+    var p_Country = request.body.Country;
+    var id = request.body.id;
+    
+
+    connection.query("CALL UpdateMovieDetails(?,?,?,?,?,?,?,?,?,?,?,?,?)", [p_Name,p_Description,p_releaseDate,p_LongMinutes,p_ImageUrl,p_Genre,p_BoxOffice,p_Writer,
+        p_Director,p_Actors,p_LanguageMovie,p_Country,id],
+     function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "Movie has been upadted successfully",
+                data: result
+            });
+        }
+    });
+});
+
+
+app.post("/user", function(request, response){
+
+    var FirstName = request.body.FirstName;
+    var LastName = request.body.LastName;
+    var Username = request.body.Username;
+    var Password = request.body.Password;
+    var Location = request.body.Location;
+    
+    
+
+    connection.query("CALL InsertUserDetails(?,?,?,?,?)", [FirstName,LastName,Username,Password,Location],
+     function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "User has been added successfully",
+                data: result
+            });
+        }
+    });
+});
+
+app.put("/user", function(request, response){
+
+    var p_FirstName = request.body.FirstName;
+    var p_LastName = request.body.LastName;
+    var p_Username = request.body.Username;
+    var PasswordIN = request.body.Password;
+    var p_Location = request.body.Location;
+    var id = request.body.id;
+    
+
+    connection.query("CALL UpdateUserDetails(?,?,?,?,?,?)", [p_FirstName,p_LastName,p_Username,PasswordIN,p_Location,id],
+     function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "User has been updated successfully",
+                data: result
+            });
+        }
+    });
+});
+
+app.post("/review", function(request, response){
+
+    var Review = request.body.Review;
+    var Rating = request.body.Rating;
+    var DateCreated = request.body.DateCreated;
+    var MovieID = request.body.MovieID;
+    var UserId = request.body.UserId;
+    
+    
+
+    connection.query("CALL InsertReview(?,?,?,?,?)", [Review,Rating,DateCreated,MovieID,UserId],
+     function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "Review has been added successfully",
+                data: result
+            });
+        }
+    });
+});
+
+app.put("/review", function(request, response){
+
+    var Review = request.body.Review;
+    var Rating = request.body.Rating;
+    var id = request.body.id;
+    
+    
+
+    connection.query("CALL UpdateReview(?,?,?)", [Review,Rating,id],
+     function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "Review has been updated successfully",
+                data: result
+            });
+        }
+    });
+});
+
+app.post("/favorite", function(request, response){
+
+    var MovieID = request.body.MovieID;
+    var UserId = request.body.UserId;
+
+    connection.query("CALL InsertFavorites(?,?)", [MovieID, UserId],
+     function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "Favorite has been added successfully",
+                data: result
+            });
+        }
+    });
+});
+
+
+app.get("/users/all", function(request, response){
+
+
+    connection.query("SELECT * FROM USER",
+     function(error, result, fields){
+
+        if(error){
+
+            throw error;
+        }else{
+
+            response.json({
+                status : 1,
+                message: "ALL USERS",
+                data: result
+            });
+        }
+    });
+});
+
+app.get("/search", function (request, response) {
+
+    // query
+    var keyword = request.body.keyword;
+    console.log(keyword);
+    connection.query(
+        "SELECT * FROM movies.getallmovieinfo WHERE MovieName LIKE ?;", [keyword],
+     function(error, results, fields){
+
+          if(error){
+              throw error;
+          }else{
+              response.json(results);
+              //console.log(fields);
+          } 
+    });
+});
+
 app.listen(PORT, function () {
 
     console.log("Server is running at 5000 port");
